c00ac1560be94f5fc1d07675dec80d7f5ddd2cf4
RyuZU-chi <kuzukenji12@gmail.com>
2022-05-26 22:12:20 +0700

    nambah notifikasi_menu.xml, notifikasi_menu_grid.xml, ganti fragment_notifikasi.xml

--- app/src/main/java/com/uty/halodocrevisi/ui/notifikasi/FragmentNotifikasi.java
+++ app/src/main/java/com/uty/halodocrevisi/ui/notifikasi/FragmentNotifikasi.java
@@ -24,8 +24,8 @@ public class FragmentNotifikasi extends Fragment {
         binding = FragmentNotifikasiBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifikasi;
-        ViewModelNotifikasi.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textNotifikasi;
+//        ViewModelNotifikasi.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
