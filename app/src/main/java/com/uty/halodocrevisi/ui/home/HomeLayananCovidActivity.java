14c574ce3b65c9ccc8dabde937b4c1ddfc591fae
RyuZU-chi <kuzukenji12@gmail.com>
2022-06-05 20:55:06 +0700

    edit doang gambar bayar sama beritacovid, sama nambah back

--- app/src/main/java/com/uty/halodocrevisi/ui/home/HomeLayananCovidActivity.java
+++ app/src/main/java/com/uty/halodocrevisi/ui/home/HomeLayananCovidActivity.java
@@ -2,8 +2,11 @@ package com.uty.halodocrevisi.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.uty.halodocrevisi.MainActivity;
 import com.uty.halodocrevisi.R;
 
 public class HomeLayananCovidActivity extends AppCompatActivity {
@@ -13,4 +16,11 @@ public class HomeLayananCovidActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_layanan_covid);
     }
+
+    public void btnBack4(View view) {
+        Intent intent = new Intent(HomeLayananCovidActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/uty/halodocrevisi/ui/home/HomeLayananReportActivity.java
+++ app/src/main/java/com/uty/halodocrevisi/ui/home/HomeLayananReportActivity.java
@@ -2,8 +2,11 @@ package com.uty.halodocrevisi.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.uty.halodocrevisi.MainActivity;
 import com.uty.halodocrevisi.R;
 
 public class HomeLayananReportActivity extends AppCompatActivity {
@@ -13,4 +16,11 @@ public class HomeLayananReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_layanan_report);
     }
+
+    public void btnBack3(View view) {
+        Intent intent = new Intent(HomeLayananReportActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
