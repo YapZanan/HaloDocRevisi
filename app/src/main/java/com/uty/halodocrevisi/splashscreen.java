15b56eacc920be8a097560f6b9df134d1d5b5478
YapZanan <asdqwe123@@A123>
2022-06-08 18:00:40 +0700

    pembenaran splashscreen dan login

---
+++ app/src/main/java/com/uty/halodocrevisi/splashscreen.java
@@ -0,0 +1,31 @@
+package com.uty.halodocrevisi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+import com.uty.halodocrevisi.ui.home.HomeLayananDokterActivity;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(splashscreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },3000);
+    }
+}
